c0826f71451cb06e7270ddd34edd844bc6a3c527
briecodes <hello@brie.work>
2018-05-14 09:48:00 -0400

    lab completed.

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-function scuberGreetingForFeet(num){
-        if num <= 400 {
-                return "This one is on me!"
-        }, else if num > 2000 && num < 2500 {
-                return "I will gladly take your thirty bucks."
-        }, else if num > 2500 {
-                return "No can do."
+function scuberGreetingForFeet(num) {
+        if (num <= 400) {
+                return 'This one is on me!';
+        } else if (num > 2000 && num < 2500) {
+                return 'I will gladly take your thirty bucks.';
+        } else {
+                return 'No can do.';
         }
-};
+}
 
 function ternaryCheckCity(city){
-        if city === "NYC" {
+        if (city === "NYC") {
                 return "Ok, sounds good."
-        }, else {
+        } else {
                 return "No go."
         }
 }
 
 function switchOnCharmFromTip(tip){
-        if tip === "generous" {
+        if (tip === "generous") {
                 return "Thank you so much."
-        }, else if tip === "not as generous" {
+        } else if (tip === "not as generous") {
                 return "Thank you."
-        }, else {
+        } else {
                 return "Bye."
         }
 }
